d160df590a5d82def53487ae937964f95f9357e9
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-10-18 18:06:56 +0300

    add empty classes

---
+++ desktop/src/rubt/DesktopLauncher.java
@@ -0,0 +1,10 @@
+package rubt;
+
+import arc.util.Log;
+
+public class DesktopLauncher {
+
+    public static void main(String[] args) {
+        Log.info("DESKTOP");
+    }
+}
---
+++ server/src/rubt/ServerLauncher.java
@@ -0,0 +1,10 @@
+package rubt;
+
+import arc.util.Log;
+
+public class ServerLauncher {
+
+    public static void main(String[] args) {
+        Log.info("SERVER");
+    }
+}
