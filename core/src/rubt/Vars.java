7d0aee51b30bc3124d4fb3edea511ccef5c9b700
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-10-20 17:38:54 +0300

    add load logger method and headless app

--- core/src/rubt/Vars.java
+++ core/src/rubt/Vars.java
@@ -1,6 +1,23 @@
 package rubt;
 
+import arc.net.ArcNet;
+import arc.util.Log;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Vars {
 
+    public static final String[] tags = { "&lc&fb[D]&fr", "&lb&fb[I]&fr", "&ly&fb[W]&fr", "&lr&fb[E]", "" };
+    public static final DateTimeFormatter dateTime = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
     public static float tilesize = 16f;
+
+    public static void loadLogger() {
+        ArcNet.errorHandler = Log::err;
+        Log.logger = (level, text) -> { // this is how fashionable i am
+            String result = Log.format("&lk&fb[" + dateTime.format(LocalDateTime.now()) + "]&fr " + tags[level.ordinal()] + " " + text + "&fr");
+            System.out.println(result);
+        };
+    }
 }
--- server/src/rubt/ServerLauncher.java
+++ server/src/rubt/ServerLauncher.java
@@ -1,10 +1,19 @@
 package rubt;
 
+import arc.ApplicationListener;
+import arc.backend.headless.HeadlessApplication;
 import arc.util.Log;
 
-public class ServerLauncher {
+public class ServerLauncher implements ApplicationListener {
 
     public static void main(String[] args) {
-        Log.info("SERVER");
+        try {
+            Vars.loadLogger();
+            new HeadlessApplication(new ServerLauncher(args), Log::err);
+        } catch (Throwable error) {
+            Log.err("Could not to startup server application", error);
+        }
     }
+
+    public ServerLauncher(String[] args) {}
 }
