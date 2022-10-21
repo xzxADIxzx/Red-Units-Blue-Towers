862ef77e555262c4f4e15ae15ab07cde80e1814e
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-10-21 16:39:41 +0300

    add sdl app and port parser

--- desktop/src/rubt/desktop/DesktopLauncher.java
+++ desktop/src/rubt/desktop/DesktopLauncher.java
@@ -1,10 +1,31 @@
 package rubt.desktop;
 
+import arc.ApplicationListener;
+import arc.backend.sdl.SdlApplication;
+import arc.backend.sdl.SdlConfig;
+import arc.backend.sdl.jni.SDL;
 import arc.util.Log;
 
-public class DesktopLauncher {
+import static rubt.Vars.*;
+
+public class DesktopLauncher implements ApplicationListener {
 
     public static void main(String[] args) {
-        Log.info("DESKTOP");
+        try {
+            loadLogger(); // needed only for debug
+            new SdlApplication(new DesktopLauncher(args), new SdlConfig() {{
+                title = "Red Units Blue Towers";
+                maximized = true;
+            }});
+        } catch (Throwable error) {
+            crushed("Oh no, critical error", error);
+        }
     }
+
+    public static void crushed(String title,Throwable error) {
+        Log.err(title, error);
+        SDL.SDL_ShowSimpleMessageBox(SDL.SDL_MESSAGEBOX_ERROR, title, error.getMessage());
+    }
+
+    public DesktopLauncher(String[] args) {}
 }
--- server/src/rubt/server/ServerLauncher.java
+++ server/src/rubt/server/ServerLauncher.java
@@ -8,6 +8,8 @@ import static rubt.Vars.*;
 
 public class ServerLauncher implements ApplicationListener {
 
+    public final int port;
+
     public static void main(String[] args) {
         try {
             loadLogger(); // pretty print
@@ -17,5 +19,8 @@ public class ServerLauncher implements ApplicationListener {
         }
     }
 
-    public ServerLauncher(String[] args) {}
+    public ServerLauncher(String[] args) {
+        if (args.length == 0) throw new RuntimeException("Need a port as an argument!");
+        port = Integer.parseInt(args[0]);
+    }
 }
