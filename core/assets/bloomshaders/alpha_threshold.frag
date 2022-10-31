953329f900f70e7c9eb88401209f638c2ec48706
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-10-31 22:42:58 +0300

    add bloom (not working)

---
+++ core/assets/bloomshaders/alpha_threshold.frag
@@ -0,0 +1,13 @@
+uniform lowp sampler2D u_texture0;
+uniform lowp vec2 threshold;
+
+varying vec2 v_texCoords;
+
+void main() {
+	vec4 color = texture2D(u_texture0, v_texCoords);
+	if (color.r + color.g + color.b > 0.5 * 3.0) {
+		gl_FragColor = color;
+	} else {
+		gl_FragColor = vec4(0.0);
+	}
+}
--- core/src/rubt/graphics/Renderer.java
+++ core/src/rubt/graphics/Renderer.java
@@ -1,6 +1,7 @@
 package rubt.graphics;
 
 import arc.graphics.Color;
+import arc.graphics.g2d.Bloom;
 import arc.graphics.g2d.Draw;
 import rubt.Groups;
 import rubt.world.*;
@@ -10,8 +11,14 @@ import static rubt.Vars.*;
 
 public class Renderer {
 
+    public Bloom bloom = new Bloom(true);
+
     public void draw() {
         camera.resize(graphics.getWidth() / 4f, graphics.getHeight() / 4f);
+        bloom.resize(graphics.getWidth(), graphics.getHeight());
+
+        bloom.setBloomIntesity(10f);
+        bloom.blurPasses = 6;
 
         Draw.proj(camera);
         Draw.sort(true);
@@ -24,6 +31,8 @@ public class Renderer {
         Draw.draw(Layers.units, () -> Groups.units.each(Unit::draw));
         Draw.draw(Layers.turrets, () -> Groups.turrets.each(Turret::draw));
 
+        Draw.draw(Layers.overlay - .02f, bloom::capture);
+        Draw.draw(Layers.overlay + .02f, bloom::render);
         Draw.draw(Layers.overlay, handler::draw);
 
         Draw.flush();
