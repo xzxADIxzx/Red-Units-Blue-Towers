28b2e23c517fdd96a0390fd8d55e04fc064a6737
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-11-27 00:03:48 +0300

    add ground unit to test pathdinfer

---
+++ core/src/rubt/types/units/GroundUnit.java
@@ -0,0 +1,26 @@
+package rubt.types.units;
+
+import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.Fill;
+import rubt.types.UnitType;
+import rubt.world.Unit;
+
+public class GroundUnit extends UnitType {
+
+    public GroundUnit(String name) {
+        super(name);
+    }
+
+    public void update(Unit unit) {
+        unit.movePath(unit.target);
+    }
+
+    public void draw(Unit unit) {
+        Draw.color(Color.red);
+        Fill.square(unit.x, unit.y, 8f, unit.rot());
+
+        Draw.color(Color.blue);
+        Fill.square(unit.target.getX(), unit.target.getY(), 8f);
+    }
+}
