24b1b11b7bbababfe75a32519f598a5eb2ad1f52
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-11-27 00:01:57 +0300

    add pathfinding to pathfinder

--- core/src/rubt/world/Pathfinder.java
+++ core/src/rubt/world/Pathfinder.java
@@ -4,12 +4,40 @@ import arc.struct.Seq;
 
 public class Pathfinder {
 
+    public static final int maxDatasSize = 1000;
+
     public Path findPath(Tile from, Tile to) {
-        var tiles = Seq.with(from);
+        PathData winner = null;
+        Seq<PathData> datas = Seq.with(PathData.from(to));
 
-        // TODO find path, huh
+        outer:
+        while (datas.size < maxDatasSize) { // if the size is more than a thousand, there is no way
 
-        return new Path(tiles.add(to));
+            var old = datas;
+            datas = new Seq<>();
+
+            for (PathData data : old) {
+                if (data.from == from) {
+                    winner = data;
+                    break outer;
+                }
+
+                datas.addAll(iteratePath(data));
+            }
+        }
+
+        return winner == null ? null : new Path(winner.tiles);
+    }
+
+    private Seq<PathData> iteratePath(PathData parent) {
+        Seq<PathData> datas = new Seq<>();
+
+        parent.from.neightbours().each(tile -> {
+            if (parent.tiles.contains(tile)) return;
+            datas.add(parent.copy(tile));
+        });
+
+        return datas;
     }
 
     public static record Path(Seq<Tile> tiles) {
@@ -17,6 +45,17 @@ public class Pathfinder {
         public Tile nextOnPath(Tile from) {
             if (tiles.peek() == from) tiles.pop();
             return tiles.peek();
+        }
+    }
+
+    public static record PathData(Seq<Tile> tiles, Tile from) {
+
+        public static PathData from(Tile tile) {
+            return new PathData(Seq.with(tile), tile);
+        }
+
+        public PathData copy(Tile tile) {
+            return new PathData(tiles.copy().add(tile), tile);
         }
     }
 }
--- core/src/rubt/world/Tile.java
+++ core/src/rubt/world/Tile.java
@@ -3,6 +3,7 @@ package rubt.world;
 import arc.graphics.Color;
 import arc.graphics.g2d.Draw;
 import arc.graphics.g2d.Fill;
+import arc.math.geom.Geometry;
 import rubt.Groups;
 import rubt.Groups.GroupObject;
 
@@ -31,5 +32,16 @@ public class Tile extends GroupObject {
 
         Draw.color(Color.gray);
         Fill.rect(drawX(), drawY(), tilesize, tilesize);
+    }
+
+    public Seq<Tile> neightbours() {
+        Seq<Tile> neightbours = new Seq<>();
+
+        for (var offset : Geometry.d4) {
+            Tile neightbour = world.get(x + offset.x, y + offset.y);
+            if (neightbour != null) neightbours.add(neightbour);
+        }
+
+        return neightbours;
     }
 }
