c302e08e280fe6c4401320b306d83c9b3397313a
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-12-06 21:33:16 +0300

    add splits and few textures

--- core/src/rubt/graphics/Textures.java
+++ core/src/rubt/graphics/Textures.java
@@ -3,15 +3,24 @@ package rubt.graphics;
 import arc.graphics.Texture;
 import arc.graphics.Texture.TextureFilter;
 import arc.scene.style.Drawable;
+import arc.util.serialization.JsonReader;
+import arc.util.serialization.JsonValue;
 import rubt.types.textures.NormalTexture;
 
 import static arc.Core.*;
 
 public class Textures {
 
-    public static Drawable whiteui, circle;
+    public static JsonValue splits;
+    public static Drawable mainbg, alphabg, accentbg, whiteui, circle;
 
     public static void load() {
+        splits = new JsonReader().parse(files.internal("sprites/ui/splits.json"));
+
+        mainbg = loadUI("mainbg");
+        alphabg = loadUI("alphabg");
+        accentbg = loadUI("accentbg");
+
         whiteui = loadUI("whiteui");
         circle = loadUI("circle");
     }
@@ -20,7 +29,8 @@ public class Textures {
         Texture texture = new Texture(path + name + ".png");
         texture.setFilter(TextureFilter.linear); // for better experience
 
-        atlas.addRegion(name, texture, 0, 0, texture.width, texture.height);
+        var region = atlas.addRegion(name, texture, 0, 0, texture.width, texture.height);
+        if (splits.has(name)) region.splits = splits.get(name).asIntArray();
     }
 
     public static Drawable loadUI(String name) {
