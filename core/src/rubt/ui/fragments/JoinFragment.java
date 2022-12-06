fd6e43de7bd7352c12e6b6573f996a6d86ebd52f
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-12-06 21:36:57 +0300

    add simple join button

--- core/src/rubt/ui/fragments/JoinFragment.java
+++ core/src/rubt/ui/fragments/JoinFragment.java
@@ -1,8 +1,27 @@
 package rubt.ui.fragments;
 
 import arc.scene.Group;
+import arc.util.Log;
+import rubt.logic.State;
+import rubt.net.Host;
+
+import static rubt.Vars.*;
 
 public class JoinFragment {
 
-    public void build(Group parent) {}
+    public void build(Group parent) {
+        parent.fill(cont -> {
+            cont.name = "Join Fragment";
+            cont.visible(() -> state == State.menu);
+
+            // TODO normal join
+            cont.button(b -> {}, () -> {
+                try {
+                    new Host("127.0.0.1", 6567).join();
+                } catch (Throwable error) {
+                    Log.err("Could not to join server", error);
+                }
+            }).size(100f, 40f);
+        });
+    }
 }
