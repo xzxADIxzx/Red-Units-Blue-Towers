9f932639b72b64f6d9a77ee3d61bb02b3f43661b
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-12-06 21:36:15 +0300

    add palette & styles

---
+++ core/src/rubt/graphics/Palette.java
@@ -0,0 +1,16 @@
+package rubt.graphics;
+
+import arc.graphics.Color;
+
+public class Palette {
+
+    public static final Color
+
+    main = Color.valueOf("282E33"),
+
+    alpha = Color.valueOf("282E33CC"),
+
+    accent = Color.valueOf("006E62")
+
+    ;
+}
--- core/src/rubt/ui/Styles.java
+++ core/src/rubt/ui/Styles.java
@@ -1,6 +1,19 @@
 package rubt.ui;
 
+import arc.scene.ui.Button.ButtonStyle;
+import rubt.graphics.Textures;
+
+import static arc.Core.*;
+
 public class Styles {
 
-    public static void load() {}
+    public static ButtonStyle button;
+
+    public static void load() {
+        scene.addStyle(ButtonStyle.class, button = new ButtonStyle() {{
+            up = Textures.alphabg;
+            over = Textures.mainbg;
+            down = Textures.accentbg;
+        }});
+    }
 }
