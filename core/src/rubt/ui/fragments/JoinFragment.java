c385fee2a50d9fbcfd126805a4ad39f8ef28d064
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-12-07 22:55:28 +0300

    add sketch for join frag

--- core/src/rubt/ui/fragments/JoinFragment.java
+++ core/src/rubt/ui/fragments/JoinFragment.java
@@ -2,6 +2,7 @@ package rubt.ui.fragments;
 
 import arc.scene.Group;
 import arc.util.Log;
+import rubt.graphics.Textures;
 import rubt.logic.State;
 import rubt.net.Host;
 
@@ -14,14 +15,33 @@ public class JoinFragment {
             cont.name = "Join Fragment";
             cont.visible(() -> state == State.menu);
 
-            // TODO normal join
-            cont.button(b -> {}, () -> {
-                try {
-                    new Host("127.0.0.1", 6567).join();
-                } catch (Throwable error) {
-                    Log.err("Could not to join server", error);
-                }
-            }).size(100f, 40f);
+            cont.margin(8f);
+
+            cont.table(list -> { // server list
+                list.name = "Server list";
+                list.defaults().height(200f).growX().padBottom(8f);
+
+                for (int i = 0; i < 4; i++)
+                    list.table(Textures.alphabg).row();
+            }).width(600f).padRight(8f);
+
+            cont.table(info -> { // nickname & server info
+                info.name = "Nickname & Server info";
+                info.defaults().height(64f).growX().padBottom(8f);
+
+                info.table(Textures.alphabg).padBottom(16f).row();
+
+                info.table(Textures.alphabg).row();
+                info.table(Textures.alphabg).height(512f).row();
+
+                info.button(b -> {}, () -> { // TODO normal join
+                    try {
+                        new Host("127.0.0.1", 6567).join();
+                    } catch (Throwable error) {
+                        Log.err("Could not to join server", error);
+                    }
+                });
+            }).growX().top();
         });
     }
 }
