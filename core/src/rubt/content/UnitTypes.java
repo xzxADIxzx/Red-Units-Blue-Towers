04ecb5b1ba94ed1f594901ee24bd74a7b85d00c7
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-12-07 22:55:37 +0300

    add ground unit

--- core/src/rubt/content/UnitTypes.java
+++ core/src/rubt/content/UnitTypes.java
@@ -3,12 +3,14 @@ package rubt.content;
 import arc.struct.Seq;
 import rubt.types.UnitType;
 import rubt.types.units.AirUnit;
+import rubt.types.units.GroundUnit;
 
 public class UnitTypes {
 
     public static Seq<UnitType> all = new Seq<>();
 
     public static AirUnit imau;
+    public static GroundUnit furbo;
 
     /** Create unit types. */
     public static void load() {
@@ -18,6 +20,14 @@ public class UnitTypes {
 
             speed = 2f;
             accel = .12f;
+        }};
+
+        furbo = new GroundUnit("furbo") {{
+            health = 200;
+            damage = 30;
+
+            speed = 1.2f;
+            accel = .1f;
         }};
     }
 
