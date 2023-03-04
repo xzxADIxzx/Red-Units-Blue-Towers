d27384a8b94ccea07b85e66812c2c4a890064fec
xzxADIxzx <valerazhuk20122006@gmail.com>
2023-03-05 00:05:31 +0300

    add base dialog

--- core/src/rubt/ui/Styles.java
+++ core/src/rubt/ui/Styles.java
@@ -2,10 +2,12 @@ package rubt.ui;
 
 import arc.graphics.Color;
 import arc.scene.ui.Button.ButtonStyle;
+import arc.scene.ui.Dialog.DialogStyle;
 import arc.scene.ui.Label.LabelStyle;
 import arc.scene.ui.ScrollPane.ScrollPaneStyle;
 import arc.scene.ui.TextButton.TextButtonStyle;
 import arc.scene.ui.TextField.TextFieldStyle;
+import rubt.graphics.Palette;
 import rubt.graphics.Textures;
 
 import static arc.Core.*;
@@ -18,6 +20,7 @@ public class Styles {
     public static LabelStyle label;
     public static TextFieldStyle field;
     public static ScrollPaneStyle scroll;
+    public static DialogStyle dialog;
 
     public static void load() {
         scene.addStyle(ButtonStyle.class, button = new ButtonStyle() {{
@@ -40,7 +43,7 @@ public class Styles {
             font = Fonts.tech;
         }});
 
-        scene.addStyle(TextFieldStyle.class, field = new TextFieldStyle() {{
+        scene.addStyle(TextFieldStyle.class, field = new TextFieldStyle() {{ // TODO invalid background with red color from palette
             font = Fonts.tech;
 
             fontColor = Color.white;
@@ -54,6 +57,13 @@ public class Styles {
             vScrollKnob = Textures.accentbg;
             hScroll = Textures.mainbg;
             hScrollKnob = Textures.accentbg;
+        }});
+
+        scene.addStyle(DialogStyle.class, dialog = new DialogStyle() {{ // TODO dark stage background
+            titleFont = Fonts.tech;
+
+            titleFontColor = Palette.accent;
+            background = Textures.alphabg;
         }});
     }
 }
---
+++ core/src/rubt/ui/dialogs/BaseDialog.java
@@ -0,0 +1,30 @@
+package rubt.ui.dialogs;
+
+import arc.scene.ui.Dialog;
+import rubt.graphics.Palette;
+
+import static arc.Core.*;
+
+public class BaseDialog extends Dialog {
+
+    public BaseDialog(String title, DialogStyle style) {
+        super(title, style);
+
+        titleTable.clear();
+        titleTable.image().color(Palette.accent).height(4f).growX();
+        titleTable.add(this.title).pad(4f);
+        titleTable.image().color(Palette.accent).height(4f).growX();
+    }
+
+    public BaseDialog(String title) {
+        this(title, scene.getStyle(DialogStyle.class));
+    }
+
+    @Override
+    public void addCloseButton() {
+        buttons.defaults().size(200f, 64f);
+        buttons.button("Back", this::hide);
+
+        closeOnBack();
+    }
+}
