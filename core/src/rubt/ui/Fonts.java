3c18744d82320654c85cacd76675cc720612f289
xzxADIxzx <valerazhuk20122006@gmail.com>
2023-05-27 18:26:45 +0300

    add new font (more readable)

--- core/src/rubt/ui/Fonts.java
+++ core/src/rubt/ui/Fonts.java
@@ -8,12 +8,20 @@ import static arc.Core.*;
 
 public class Fonts {
 
-    public static Font tech;
+    public static Font tech, aldrich;
 
     public static void load() {
         tech = load("tech", new FreeTypeFontParameter() {{ size = 18; }});
         tech.getData().markupEnabled = true;
         tech.getData().down *= 1.5f;
+
+        // this font is licensed under the SIL Open Font License
+        // designed by MADType, russian chars by Daymarius
+        aldrich = load("aldrich", new FreeTypeFontParameter() {{
+            size = 20;
+            characters += "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя";
+        }});
+        aldrich.getData().markupEnabled = true;
     }
 
     public static Font load(String name, FreeTypeFontParameter parameter) {
