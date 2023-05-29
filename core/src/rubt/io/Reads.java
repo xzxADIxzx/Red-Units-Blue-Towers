800c5c63bc34fbec0d1f6ff5842f70bc5b375c9e
xzxADIxzx <valerazhuk20122006@gmail.com>
2023-05-29 22:02:51 +0300

    add better writes/reads

---
+++ core/src/rubt/io/Reads.java
@@ -0,0 +1,37 @@
+package rubt.io;
+
+import arc.math.geom.Position;
+import arc.math.geom.Vec2;
+import arc.util.io.ByteBufferInput;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+
+/** Builtin reads but with some extra methods for convenience. */
+public class Reads extends arc.util.io.Reads {
+
+    private static Reads instance = new Reads();
+
+    public Reads() {
+        super(null);
+    }
+
+    public static Reads of(DataInput output) {
+        instance.input = output;
+        return instance;
+    }
+
+    public static Reads of(InputStream output) {
+        instance.input = new DataInputStream(output);
+        return instance;
+    }
+
+    public static Reads of(ByteBuffer buffer) {
+        instance.input = new ByteBufferInput(buffer);
+        return instance;
+    }
+
+    public Position p() {
+        return new Vec2(f(), f());
+    }
+}
---
+++ core/src/rubt/io/Writes.java
@@ -0,0 +1,37 @@
+package rubt.io;
+
+import arc.math.geom.Position;
+import arc.util.io.ByteBufferOutput;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+
+/** Builtin writes but with some extra methods for convenience. */
+public class Writes extends arc.util.io.Writes {
+
+    private static Writes instance = new Writes();
+
+    public Writes() {
+        super(null);
+    }
+
+    public static Writes of(DataOutput output) {
+        instance.output = output;
+        return instance;
+    }
+
+    public static Writes of(OutputStream output) {
+        instance.output = new DataOutputStream(output);
+        return instance;
+    }
+
+    public static Writes of(ByteBuffer buffer) {
+        instance.output = new ByteBufferOutput(buffer);
+        return instance;
+    }
+
+    public void p(Position pos) {
+        f(pos.getX());
+        f(pos.getY());
+    }
+}
