82d54482d2b742873081f46c3a2bf13833bcd5e1
xzxADIxzx <valerazhuk20122006@gmail.com>
2023-06-29 16:33:33 +0300

    add rebuilding for menu frag

--- core/src/rubt/ui/fragments/MenuFragment.java
+++ core/src/rubt/ui/fragments/MenuFragment.java
@@ -9,6 +9,7 @@ import arc.scene.ui.layout.Table;
 import rubt.graphics.Palette;
 import rubt.graphics.Textures;
 import rubt.logic.State;
+import rubt.net.Net;
 import rubt.ui.Icons;
 import rubt.ui.Styles;
 
@@ -31,21 +32,9 @@ public class MenuFragment {
                 pane.name = "Pane";
                 pane.defaults().width(256f).padBottom(8f);
 
-                pane.table(title -> {
-                    title.image().color(Palette.red).height(4f).growX();
-                    title.add("[#FF0040]RU[#0040FF]BT", Styles.tech).pad(4f);
-                    title.image().color(Palette.blue).height(4f).growX();
-                }).row();
-
-                Cons<TextButton> anim = b -> b.translation.x = Mathf.lerpDelta(b.translation.x, b.hasMouse() ? 24f : 0f, .1f);
-
-                pane.button("Play", Icons.play, ui.joinfrag::show).update(anim).row();
-                pane.button("Editor", Icons.design, () -> ui.openEditor.show()).update(anim).row();
-                pane.button("Settings", Icons.settings, () -> {}).update(anim).row();
-                pane.button("Exit", Icons.exit, app::exit).update(anim);
-
                 this.cont = cont;
                 this.pane = pane;
+                rebuild();
             }).growY();
         });
     }
@@ -58,5 +47,33 @@ public class MenuFragment {
     public void rebuild() {
         cont.background(state == State.game || state == State.editor ? Textures.dark : null);
         cont.touchable = Touchable.enabled; // control block while the player is in the menu
+
+        pane.clear();
+
+        pane.table(title -> {
+            title.image().color(Palette.red).height(4f).growX();
+            title.add("[#FF0040]RU[#0040FF]BT", Styles.tech).pad(4f);
+            title.image().color(Palette.blue).height(4f).growX();
+        }).row();
+
+        Cons<TextButton> anim = b -> b.translation.x = Mathf.lerpDelta(b.translation.x, b.hasMouse() ? 24f : 0f, .1f);
+
+        if (state == State.menu)
+            pane.button("Play", Icons.play, ui.joinfrag::show).update(anim).row();
+        else
+            pane.button("Continue", Icons.play, this::toggle).update(anim).row();
+
+        pane.button("Editor", Icons.design, () -> ui.openEditor.show()).update(anim).row();
+        pane.button("Settings", Icons.settings, () -> {}).update(anim).row();
+
+        if (state == State.menu)
+            pane.button("Exit", Icons.exit, app::exit).update(anim);
+        else
+            pane.button("Quit", Icons.close, () -> { // TODO replace with dialog::show
+                if (state == State.game) Net.disconnect();
+                if (state == State.editor) state = State.menu;
+                toggle();
+                rebuild();
+            });
     }
 }
