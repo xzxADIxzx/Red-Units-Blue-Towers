ca6d21ebbd676aabdb1f2adb77858de8e8206ff5
xzxADIxzx <valerazhuk20122006@gmail.com>
2023-06-29 20:11:51 +0300

    add a class to interpolate floats

---
+++ core/src/rubt/net/FloatLerp.java
@@ -0,0 +1,25 @@
+package rubt.net;
+
+import arc.math.Mathf;
+import arc.util.Time;
+import arc.util.io.Reads;
+import rubt.logic.Logic;
+
+public class FloatLerp {
+
+    public static float updateSpacing = 1000f / Logic.litsFrequency;
+    public float last, target;
+
+    public void set(float value) {
+        last = target;
+        target = value;
+    }
+
+    public void read(Reads r) {
+        set(r.f());
+    }
+
+    public float get(long lastUpdate) {
+        return Mathf.lerp(last, target, Time.timeSinceMillis(lastUpdate) / updateSpacing);
+    }
+}
