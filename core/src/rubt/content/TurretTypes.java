ffb7499f72c3c5180de702974ba96bccb6ac047b
xzxADIxzx <valerazhuk20122006@gmail.com>
2023-07-02 15:36:38 +0300

    add bullets for turrets

--- core/src/rubt/content/TurretTypes.java
+++ core/src/rubt/content/TurretTypes.java
@@ -2,6 +2,7 @@ package rubt.content;
 
 import arc.struct.Seq;
 import rubt.types.TurretType;
+import rubt.types.bullets.BaseBullet;
 import rubt.types.turrets.HeadTurret;
 
 public class TurretTypes {
@@ -18,6 +19,12 @@ public class TurretTypes {
 
             rotationSpeed = 6f;
             reload = 40f;
+
+            bullet = new BaseBullet("dislike-bullet") {{
+                damage = 1f;
+                size = 1f;
+                speed = 1f;
+            }};
         }};
 
         imat = new HeadTurret("imat") {{
@@ -28,6 +35,13 @@ public class TurretTypes {
             burstShoots = 4;
             burstCooldown = 120f;
             inaccuracy = 5f;
+
+            bullet = new BaseBullet("imat-bullet") {{
+                damage = 3f;
+                size = 1.8f;
+                speed = 3f;
+                pierce = 2;
+            }};
         }};
     }
 
